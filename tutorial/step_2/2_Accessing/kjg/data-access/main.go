fed3ae5fbbd8d925906914a3bdc3f9b861c6262a
김재곤 <kjg@ilevit.com>
2024-11-27 00:53:39 +0900

    update

--- tutorial/step_2/2_Accessing/kjg/data-access/main.go
+++ tutorial/step_2/2_Accessing/kjg/data-access/main.go
@@ -11,6 +11,7 @@ import (
 
 var db *sql.DB
 
+// Album 구조체 정의
 type Album struct {
 	ID     int64
 	Title  string
@@ -18,57 +19,52 @@ type Album struct {
 	Price  float32
 }
 
-func main() {
-
-		// 데이터베이스 연결 설정
-    cfg := mysql.Config{
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "recordings",
-    }
-    var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
-    fmt.Println("Connected!")
-
-		//albumsByArtist 함수 테스트
-    albums, err := albumsByArtist("John Coltrane")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("Albums found: %v\n", albums)		
-}
-
-// albumsByArtist queries for albums that have the specified artist name.
+// 다중 행 쿼리 함수
 func albumsByArtist(name string) ([]Album, error) {
-	// An albums slice to hold data from returned rows.
 	var albums []Album
 
 	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
 	if err != nil {
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	defer rows.Close()
 
-	// Loop through rows, using Scan to assign column data to struct fields.
 	for rows.Next() {
-			var alb Album
-			if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-					return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-			}
-			albums = append(albums, alb)
-	}
-	if err := rows.Err(); err != nil {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
 			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
 	}
+
 	return albums, nil
+}
+
+func main() {
+	// 데이터베이스 연결 설정
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected!")
+
+	// 다중 행 쿼리 테스트
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
 }
